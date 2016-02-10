4650bbbfe3958a07c3221c126a61e8ad10df2057
Roodey <joey8williams@gmail.com>
2016-02-10 16:19:39 -0500

    background color, slow and steady

---
+++ FrontEnd/dist/assets/awesome-schedule-front.js
@@ -0,0 +1,165 @@
+"use strict";
+/* jshint ignore:start */
+
+/* jshint ignore:end */
+
+define('awesome-schedule-front/app', ['exports', 'ember', 'ember/resolver', 'ember/load-initializers', 'awesome-schedule-front/config/environment'], function (exports, _ember, _emberResolver, _emberLoadInitializers, _awesomeScheduleFrontConfigEnvironment) {
+
+  var App = undefined;
+
+  _ember['default'].MODEL_FACTORY_INJECTIONS = true;
+
+  App = _ember['default'].Application.extend({
+    modulePrefix: _awesomeScheduleFrontConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _awesomeScheduleFrontConfigEnvironment['default'].podModulePrefix,
+    Resolver: _emberResolver['default']
+  });
+
+  (0, _emberLoadInitializers['default'])(App, _awesomeScheduleFrontConfigEnvironment['default'].modulePrefix);
+
+  exports['default'] = App;
+});
+define('awesome-schedule-front/components/app-version', ['exports', 'ember-cli-app-version/components/app-version', 'awesome-schedule-front/config/environment'], function (exports, _emberCliAppVersionComponentsAppVersion, _awesomeScheduleFrontConfigEnvironment) {
+
+  var name = _awesomeScheduleFrontConfigEnvironment['default'].APP.name;
+  var version = _awesomeScheduleFrontConfigEnvironment['default'].APP.version;
+
+  exports['default'] = _emberCliAppVersionComponentsAppVersion['default'].extend({
+    version: version,
+    name: name
+  });
+});
+define('awesome-schedule-front/controllers/array', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Controller;
+});
+define('awesome-schedule-front/controllers/object', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Controller;
+});
+define('awesome-schedule-front/initializers/app-version', ['exports', 'ember-cli-app-version/initializer-factory', 'awesome-schedule-front/config/environment'], function (exports, _emberCliAppVersionInitializerFactory, _awesomeScheduleFrontConfigEnvironment) {
+  exports['default'] = {
+    name: 'App Version',
+    initialize: (0, _emberCliAppVersionInitializerFactory['default'])(_awesomeScheduleFrontConfigEnvironment['default'].APP.name, _awesomeScheduleFrontConfigEnvironment['default'].APP.version)
+  };
+});
+define('awesome-schedule-front/initializers/export-application-global', ['exports', 'ember', 'awesome-schedule-front/config/environment'], function (exports, _ember, _awesomeScheduleFrontConfigEnvironment) {
+  exports.initialize = initialize;
+
+  function initialize() {
+    var application = arguments[1] || arguments[0];
+    if (_awesomeScheduleFrontConfigEnvironment['default'].exportApplicationGlobal !== false) {
+      var value = _awesomeScheduleFrontConfigEnvironment['default'].exportApplicationGlobal;
+      var globalName;
+
+      if (typeof value === 'string') {
+        globalName = value;
+      } else {
+        globalName = _ember['default'].String.classify(_awesomeScheduleFrontConfigEnvironment['default'].modulePrefix);
+      }
+
+      if (!window[globalName]) {
+        window[globalName] = application;
+
+        application.reopen({
+          willDestroy: function willDestroy() {
+            this._super.apply(this, arguments);
+            delete window[globalName];
+          }
+        });
+      }
+    }
+  }
+
+  exports['default'] = {
+    name: 'export-application-global',
+
+    initialize: initialize
+  };
+});
+define('awesome-schedule-front/router', ['exports', 'ember', 'awesome-schedule-front/config/environment'], function (exports, _ember, _awesomeScheduleFrontConfigEnvironment) {
+
+  var Router = _ember['default'].Router.extend({
+    location: _awesomeScheduleFrontConfigEnvironment['default'].locationType
+  });
+
+  Router.map(function () {});
+
+  exports['default'] = Router;
+});
+define("awesome-schedule-front/templates/application", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template((function () {
+    return {
+      meta: {
+        "revision": "Ember@1.13.12",
+        "loc": {
+          "source": null,
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 4,
+            "column": 0
+          }
+        },
+        "moduleName": "awesome-schedule-front/templates/application.hbs"
+      },
+      arity: 0,
+      cachedFragment: null,
+      hasRendered: false,
+      buildFragment: function buildFragment(dom) {
+        var el0 = dom.createDocumentFragment();
+        var el1 = dom.createElement("h2");
+        dom.setAttribute(el1, "id", "title");
+        var el2 = dom.createTextNode("Under Development");
+        dom.appendChild(el1, el2);
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n\n");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createComment("");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n");
+        dom.appendChild(el0, el1);
+        return el0;
+      },
+      buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
+        var morphs = new Array(1);
+        morphs[0] = dom.createMorphAt(fragment, 2, 2, contextualElement);
+        return morphs;
+      },
+      statements: [["content", "outlet", ["loc", [null, [3, 0], [3, 10]]]]],
+      locals: [],
+      templates: []
+    };
+  })());
+});
+/* jshint ignore:start */
+
+/* jshint ignore:end */
+
+/* jshint ignore:start */
+
+define('awesome-schedule-front/config/environment', ['ember'], function(Ember) {
+  var prefix = 'awesome-schedule-front';
+/* jshint ignore:start */
+
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = Ember['default'].$('meta[name="' + metaName + '"]').attr('content');
+  var config = JSON.parse(unescape(rawConfig));
+
+  return { 'default': config };
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+/* jshint ignore:end */
+
+});
+
+if (!runningTests) {
+  require("awesome-schedule-front/app")["default"].create({"name":"awesome-schedule-front","version":"0.0.0+ae9a28e4"});
+}
+
+/* jshint ignore:end */
+//# sourceMappingURL=awesome-schedule-front.map
---
+++ FrontEnd/dist/assets/tests.js
@@ -0,0 +1,133 @@
+define('awesome-schedule-front/tests/app.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - .');
+  QUnit.test('app.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(false, 'app.js should pass jshint.\napp.js: line 1, col 1, \'import\' is only available in ES6 (use \'esversion: 6\').\napp.js: line 2, col 1, \'import\' is only available in ES6 (use \'esversion: 6\').\napp.js: line 3, col 1, \'import\' is only available in ES6 (use \'esversion: 6\').\napp.js: line 4, col 1, \'import\' is only available in ES6 (use \'esversion: 6\').\napp.js: line 6, col 1, \'let\' is available in ES6 (use \'esversion: 6\') or Mozilla JS extensions (use moz).\napp.js: line 13, col 3, \'object short notation\' is available in ES6 (use \'esversion: 6\') or Mozilla JS extensions (use moz).\napp.js: line 18, col 1, \'export\' is only available in ES6 (use \'esversion: 6\').\n\n7 errors');
+  });
+});
+define('awesome-schedule-front/tests/helpers/destroy-app', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = destroyApp;
+
+  function destroyApp(application) {
+    _ember['default'].run(application, 'destroy');
+  }
+});
+define('awesome-schedule-front/tests/helpers/destroy-app.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - helpers');
+  QUnit.test('helpers/destroy-app.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/destroy-app.js should pass jshint.');
+  });
+});
+define('awesome-schedule-front/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'awesome-schedule-front/tests/helpers/start-app', 'awesome-schedule-front/tests/helpers/destroy-app'], function (exports, _qunit, _awesomeScheduleFrontTestsHelpersStartApp, _awesomeScheduleFrontTestsHelpersDestroyApp) {
+  exports['default'] = function (name) {
+    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+    (0, _qunit.module)(name, {
+      beforeEach: function beforeEach() {
+        this.application = (0, _awesomeScheduleFrontTestsHelpersStartApp['default'])();
+
+        if (options.beforeEach) {
+          options.beforeEach.apply(this, arguments);
+        }
+      },
+
+      afterEach: function afterEach() {
+        (0, _awesomeScheduleFrontTestsHelpersDestroyApp['default'])(this.application);
+
+        if (options.afterEach) {
+          options.afterEach.apply(this, arguments);
+        }
+      }
+    });
+  };
+});
+define('awesome-schedule-front/tests/helpers/module-for-acceptance.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - helpers');
+  QUnit.test('helpers/module-for-acceptance.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/module-for-acceptance.js should pass jshint.');
+  });
+});
+define('awesome-schedule-front/tests/helpers/resolver', ['exports', 'ember/resolver', 'awesome-schedule-front/config/environment'], function (exports, _emberResolver, _awesomeScheduleFrontConfigEnvironment) {
+
+  var resolver = _emberResolver['default'].create();
+
+  resolver.namespace = {
+    modulePrefix: _awesomeScheduleFrontConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _awesomeScheduleFrontConfigEnvironment['default'].podModulePrefix
+  };
+
+  exports['default'] = resolver;
+});
+define('awesome-schedule-front/tests/helpers/resolver.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - helpers');
+  QUnit.test('helpers/resolver.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/resolver.js should pass jshint.');
+  });
+});
+define('awesome-schedule-front/tests/helpers/start-app', ['exports', 'ember', 'awesome-schedule-front/app', 'awesome-schedule-front/config/environment'], function (exports, _ember, _awesomeScheduleFrontApp, _awesomeScheduleFrontConfigEnvironment) {
+  exports['default'] = startApp;
+
+  function startApp(attrs) {
+    var application = undefined;
+
+    var attributes = _ember['default'].merge({}, _awesomeScheduleFrontConfigEnvironment['default'].APP);
+    attributes = _ember['default'].merge(attributes, attrs); // use defaults, but you can override;
+
+    _ember['default'].run(function () {
+      application = _awesomeScheduleFrontApp['default'].create(attributes);
+      application.setupForTesting();
+      application.injectTestHelpers();
+    });
+
+    return application;
+  }
+});
+define('awesome-schedule-front/tests/helpers/start-app.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - helpers');
+  QUnit.test('helpers/start-app.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/start-app.js should pass jshint.');
+  });
+});
+define('awesome-schedule-front/tests/router.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - .');
+  QUnit.test('router.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(false, 'router.js should pass jshint.\nrouter.js: line 1, col 1, \'import\' is only available in ES6 (use \'esversion: 6\').\nrouter.js: line 2, col 1, \'import\' is only available in ES6 (use \'esversion: 6\').\nrouter.js: line 4, col 1, \'const\' is available in ES6 (use \'esversion: 6\') or Mozilla JS extensions (use moz).\nrouter.js: line 11, col 1, \'export\' is only available in ES6 (use \'esversion: 6\').\n\n4 errors');
+  });
+});
+define('awesome-schedule-front/tests/test-helper', ['exports', 'awesome-schedule-front/tests/helpers/resolver', 'ember-qunit'], function (exports, _awesomeScheduleFrontTestsHelpersResolver, _emberQunit) {
+
+  (0, _emberQunit.setResolver)(_awesomeScheduleFrontTestsHelpersResolver['default']);
+});
+define('awesome-schedule-front/tests/test-helper.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - .');
+  QUnit.test('test-helper.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass jshint.');
+  });
+});
+/* jshint ignore:start */
+
+require('awesome-schedule-front/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+
+/* jshint ignore:end */
+//# sourceMappingURL=tests.map
