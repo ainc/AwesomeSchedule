c872b401774a4aceaded16648af72d3a1197355c
Michael Ho <michaelho3624@gmail.com>
2016-03-22 12:50:23 -0400

    work on solution where any access to admin data is denied based on cookie

--- API/controllers/routes/basic_auth.js
+++ API/controllers/routes/basic_auth.js
@@ -2,5 +2,12 @@ var auth = database('basic-auth');
 
 module.exports = function(req,res) {
 
+/*
+    var login = auth.authenticate().then(function(value) {
+        res.cookie('some_name', 'role', { expires: new Date(Date.now() + 60000) });
+    }, function(value) {
+        res.send('failure');
+    });
+*/
 
 }
