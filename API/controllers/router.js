4704d638445cbfb60f98466f33874ed881b06248
Michael Ho <michaelho3624@gmail.com>
2016-03-22 18:03:58 -0400

    Basic implementation of cookies

--- API/controllers/router.js
+++ API/controllers/router.js
@@ -1,14 +1,12 @@
 var express = require("express");
+var app = express();
+
 var router = express.Router();
 var bodyParser = require("body-parser");
+
 
 router.post('/api/test', require('./routes/test'));
 router.post('/api/basic_auth', require('./routes/basic_auth'));
 
-// Admin only routes
-
-app.use('/api/admin', require('./routes/admin'));
-
-//router.post('/api/check_admin', require('./routes/check_admin'));
 
 module.exports = router;
--- API/controllers/routes/basic_auth.js
+++ API/controllers/routes/basic_auth.js
@@ -2,6 +2,7 @@ var auth = database('basic-auth');
 
 module.exports = function(req,res) {
 
+    console.log("Cookies: ", req.cookies);
 /*
     var login = auth.authenticate().then(function(value) {
         res.cookie('some_name', 'role', { expires: new Date(Date.now() + 60000) });
