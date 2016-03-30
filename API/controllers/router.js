ebb1233a73b7b204a620e48ff8f474b8d8cedd33
Michael Ho <michaelho3624@gmail.com>
2016-03-30 12:35:23 -0400

    Added date route for Joey

--- API/controllers/router.js
+++ API/controllers/router.js
@@ -7,6 +7,6 @@ var bodyParser = require("body-parser");
 
 router.post('/api/test', require('./routes/test'));
 router.post('/api/basic_auth', require('./routes/basic_auth'));
-
+router.post('/api/gimmieDatDate', require('./routes/gimmieDatDate'))
 
 module.exports = router;
--- API/controllers/routes/test.js
+++ API/controllers/routes/test.js
@@ -1,6 +1,6 @@
 module.exports = function(req,res) {
 
-    res.cookie('some_name', 'role', { expire: new Date(Date.now() + 60000), httpOnly: false }).send("Cookie has been sent");
+    res.cookie('some_name', 'role', { expire: new Date(Date.now() + 120000), httpOnly: false }).send("Cookie has been sent");
     
     console.log("Boom Shacka lacka!");
 }
