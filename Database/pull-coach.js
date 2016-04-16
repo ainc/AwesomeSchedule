2f6fb089abb859e3878d19ed1771bd9084ef0b66
Kevin Landwehr <bystander_001@yahoo.com>
2016-04-16 12:06:22 -0400

    Update pull-coach.js

--- Database/pull-coach.js
+++ Database/pull-coach.js
@@ -1,1 +1,22 @@
+var provider = require('./mysqlProvider.js');
 
+module.exports.authenticate = function(user, pass){
+    return new provider.RSVP.Promise(function(resolve, reject) {
+        
+        provider.connection.query('SELECT * FROM coaches WHERE username = ' + provider.connection.escape(user) + ' AND password = ' + provider.connection.escape(pass), function(err, rows) {
+            
+            if (err){ throw err;}
+            if (!rows.length)
+            {
+                
+                reject("fail");
+            }
+            else
+            {
+                resolve(rows);
+            }
+
+        });
+    });
+
+}
