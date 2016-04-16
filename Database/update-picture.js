b1a6db26d6efe0a6363c200ac88e05bba877ac27
Kevin Landwehr <bystander_001@yahoo.com>
2016-04-16 13:05:59 -0400

    Update update-picture.js

--- Database/update-picture.js
+++ Database/update-picture.js
@@ -1,7 +1,7 @@
 
 var provider = require('./mysqlProvider.js');
 
-module.exports.pullcoach = function(CoachID, username){
+module.exports.updatepicture = function(CoachID, username){
     return new provider.RSVP.Promise(function(resolve, reject) {
         
         provider.connection.query('UPDATE coaches SET picture = '/assets/images/profile_pictures/' + provider.connection.escape(username) WHERE CoID= ' + provider.connection.escape(CoachID), function(err, rows) {
