7b47a57768a6019ad718abac2700358cf0c700aa
Kevin Landwehr <bystander_001@yahoo.com>
2016-04-18 19:29:07 -0400

    Update pull-classes.js
    updated to exclude the need for function passing.

--- Database/pull-classes.js
+++ Database/pull-classes.js
@@ -1,9 +1,9 @@
 var provider = require('./mysqlProvider.js');
 
-module.exports.pullclasses = function(date){
+module.exports.pullclasses = function(){
     return new provider.RSVP.Promise(function(resolve, reject) {
         
-        provider.connection.query('SELECT * FROM classes WHERE classStartTime >= CURDATE() AND classStartTime <= ' + provider.connection.escape(date), function(err, rows) {
+        provider.connection.query('SELECT * FROM classes WHERE classStartTime >= CURDATE() AND classStartTime <= DATE_ADD(CURDATE(), INTERVAL 1 MONTH)', function(err, rows) {
             
             if (err){ throw err;}
             if (!rows.length)
