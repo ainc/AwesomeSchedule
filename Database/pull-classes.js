5f3df1f30d144f35eb4127799b125f89df78190f
Kevin Landwehr <bystander_001@yahoo.com>
2016-04-18 19:22:15 -0400

    Update pull-classes.js

--- Database/pull-classes.js
+++ Database/pull-classes.js
@@ -1,9 +1,9 @@
 var provider = require('./mysqlProvider.js');
 
-module.exports.pullclasses = function(date1, date2){
+module.exports.pullclasses = function(date){
     return new provider.RSVP.Promise(function(resolve, reject) {
         
-        provider.connection.query('SELECT * FROM classes WHERE classStartTime >= ' + provider.connection.escape(date1)' AND classStartTime <= ' + provider.connection.escape(date2), function(err, rows) {
+        provider.connection.query('SELECT * FROM classes WHERE classStartTime >= CURDATE() AND classStartTime <= ' + provider.connection.escape(date), function(err, rows) {
             
             if (err){ throw err;}
             if (!rows.length)
