b49b0e6532a7f4763180fa2c9ca73899068efd84
Kevin Landwehr <bystander_001@yahoo.com>
2016-04-20 12:58:58 -0400

    Update basic-auth.js
    added comments

--- Database/basic-auth.js
+++ Database/basic-auth.js
@@ -1,3 +1,7 @@
+//This file is called at login. It is looking for a single row of data with the username and password that have been passed into this function.
+//If we get exactly one row then the user can login.
+//If no rows are retrieved then either the username or password was incorrect.
+//If more than one row is retrieved we have a huge problem, namley that we have non-unique usernames (This should not be allowed).
 var provider = require('./mysqlProvider.js');
 
 module.exports.authenticate = function(user, pass){
