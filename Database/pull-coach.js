37ac3214cb7cac2ed5826c69e8c5ee514ab44648
Kevin Landwehr <bystander_001@yahoo.com>
2016-04-20 12:56:25 -0400

    added comments

--- Database/pull-coach.js
+++ Database/pull-coach.js
@@ -1,3 +1,5 @@
+//This file is called when we want to view coach information such as in the coaches page. The CoID is retrieved at login so passing it to this file
+//allows us to narrow the search field by knowing something about the user before hand.
 var provider = require('./mysqlProvider.js');
 
 module.exports.pullcoach = function(CoachID){
