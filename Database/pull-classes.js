827e5b66e91d1ef9cdcb447906923b0900e9c04d
Kevin Landwehr <bystander_001@yahoo.com>
2016-04-20 14:36:42 -0400

    Update pull-classes.js
    edited to keep up with DB

--- Database/pull-classes.js
+++ Database/pull-classes.js
@@ -7,7 +7,7 @@ module.exports.pullclasses = function(){
     return new provider.RSVP.Promise(function(resolve, reject) {
         // change the values under the plus sign to go forward or backword in time.
         //----------------------------------------------------------------------------------------------------+-----------------------------------------------------------+------
-        provider.connection.query('SELECT * FROM classes WHERE classStartTime >= DATE_ADD(CURDATE(), INTERVAL 0 MONTH) AND classStartTime <= DATE_ADD(CURDATE(), INTERVAL 1 MONTH)', function(err, rows) {
+        provider.connection.query('SELECT * FROM classes WHERE classDate >= DATE_ADD(CURDATE(), INTERVAL 0 MONTH) AND classDate <= DATE_ADD(CURDATE(), INTERVAL 1 MONTH)', function(err, rows) {
             
             if (err){ throw err;}
             if (!rows.length)
